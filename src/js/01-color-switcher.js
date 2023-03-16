78c6004942de28947fadafa1b19d956da00e311d
Hanna.Lytvynenko <ani-k-a@ukr.net>
2023-03-16 07:53:13 +0100

    add task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,2 +1,27 @@
 
-console.log('hello')
+const body = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+
+let timerId = null;
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+const onStartBtnClick = () => {
+timerId = setInterval(()=> {
+    body.style.backgroundColor =  getRandomHexColor();
+}, 1000)
+startBtn.disabled = true;
+
+}
+
+const onStopBtnClick = () => {
+ clearInterval(timerId);
+ startBtn.disabled = false;
+}
+
+
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
