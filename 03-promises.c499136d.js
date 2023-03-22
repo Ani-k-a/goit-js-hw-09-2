b0c01a391b5e13d3cd86e1a66ef58f450d7292d5
Ani-k-a <97547547+Ani-k-a@users.noreply.github.com>
2023-03-22 20:11:39 +0000

    Deploying to gh-pages from @ Ani-k-a/goit-js-hw-09-2@d7a3301ca1d2f6e957e229ad4cf6fb975f75deaa 🚀

--- 03-promises.c499136d.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".form"),t=null;e.addEventListener("submit",(()=>{t=setInterval((()=>{Math.random()}),e.step)}));
-//# sourceMappingURL=03-promises.c499136d.js.map
--- 03-promises.e5ed5ee9.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector(".form");t.addEventListener("submit",(function(){setInterval((function(){Math.random()}),t.step)}))}();
-//# sourceMappingURL=03-promises.e5ed5ee9.js.map
